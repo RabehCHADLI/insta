0379d4e9a7694add3e13dd565e491fca1b70956a
RabehCHADLI <rabehchl42@gmail.com>
2024-01-22 10:51:26 +0100

    start

---
+++ htdocs/config/connexion/connexion.php
@@ -0,0 +1,15 @@
+<?php
+try {
+    $connexion = new PDO("mysql:host=127.0.0.1;port=3306;dbname=instabase", 'root', '', [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    ]);
+} catch (\Throwable $th) {
+    die('erreur db');
+}
+
+
+
+
+
+
+?>
