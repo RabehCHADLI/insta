1102dda2e51c36036ff7e4d5ee423ed1b875ddf8
Orlanesthesie <orlane.r.6@gmail.com>
2024-01-22 11:04:02 +0100

    orlane

---
+++ htdocs/navbar.php
@@ -0,0 +1,32 @@
+<?php 
+include "./partials/header.php"
+?>
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">LOGO</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#">PROFIL</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">CREATE</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">FIL</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="#">EXIT</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<?php 
+include "./partials/footer.php"
+?>
