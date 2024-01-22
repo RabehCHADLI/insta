c5a55df45853e0c6e11573d957689419b6b30f12
Orlanesthesie <orlane.r.6@gmail.com>
2024-01-22 15:56:01 +0100

    orlane

--- htdocs/index.php
+++ htdocs/index.php
@@ -2,40 +2,19 @@
 include './partial/header.php';
 ?>
 
-<form action="./process/add_user_connexion.php" method="post">
-<input type="text"name='pseudo'>
-<button type='submit'>ici</button>
 
-
-
-</form>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+<div class="container border border-light border-2 rounded-2 w-25 p-5">
+  <a class="" href="./assets/images/logo.png">
+    <img src="./assets/images/logo.png" alt="logo" class="d-block">
+  </a>
+  <form action="./process/add_user_connexion.php" method="post">
+    <div class="mb-3 text-center">
+      <label for="pseudo" class="form-label "></label>
+      <input type="text" class="form-control rounded-pill" name="pseudo" id="pseudo" placeholder="Entrez votre pseudo">
+    </div>
+    <button type="submit" class="btn btn-outline-danger">Connexion</button>
+  </form>
+</div>
 
 <?php
 include './partial/footer.php';
